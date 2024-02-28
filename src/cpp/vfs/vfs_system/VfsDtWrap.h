71b37e48279fe98bdab631dea74cba0b9a3d8a6a
Hugo Leclerc <hugal.leclerc@gmail.com>
2024-02-28 16:56:28 +0100

    Wrap

--- src/cpp/vfs/vfs_system/VfsDtWrap.h
+++ src/cpp/vfs/vfs_system/VfsDtWrap.h
@@ -1,32 +1,42 @@
 #pragma once
 
-#include "VfsFunc.h" // IWYU pragma: export
+#include "VfsDtType.h"
+#include <memory>
 
 BEG_VFS_NAMESPACE
-class Type;
 
-/// raw content of VfsDtObjects (like `Scalar`, `Type`, ...)
-/// => store type info + some room for inline data (that can be a pointer to the content if data_size is not large enough)
-template<class Object_, int size_ = 3 * sizeof( void * ), int alig_ = alignof( void * )>
-class alignas( alig_) VfsDtWrap {
-public:
-    static constexpr int data_size                = ( 2 * sizeof( PI32 ) + alig_ - 1 ) / alig_ * alig_ - 2 * sizeof( PI32 ) + size_;
-    using                Object                   = Object_;
-    static constexpr int alig                     = alig_;
+/// What VfsDt objects actually store
+template<class Object,class Content>
+struct VfsDtWrap_ {
+    /// inline storage
+    struct alignas( Object::VfsDtSpec::alig ) DataDirect {
+        /**/                     DataDirect( auto &&...ctor_args ) : content( FORWARD( ctor_args )... ) {}
+        auto*                    ptr       () const { return &content; }
 
-    TT void              construct                ( FromTypeAndCtorArguments, CtType<T>, auto &&...ctor_args ); ///< with ( ctor_args... ) (and not {})
-    TT void              construct                ( FromPointer, T &&pointer );
-    TT void              construct                ( FromValue, T &&value );
+        mutable PI32             instantiated_type_index; ///<
+        PI32                     global_type_index;       ///<
+        Content                  content;                 ///<
+    };
 
-    static auto          get_compilation_flags    ( CompilationFlags &cn ) { cn.add_inc_file( "vfs/vfs_system/VfsDt.h" ); }
-    static void          for_each_template_arg    ( auto &&f ) { f( CtType<Object>() ); f( CtInt<size_>() ); f( CtInt<alig_>() ); }
-    static auto          template_type_name       () { return "VfsDt"; }
+    /// indirect storage
+    struct alignas( Object::VfsDtSpec::alig ) DataHeap {
+        /**/                     DataHeap  ( auto &&...ctor_args ) : content( std::make_unique<Content>( FORWARD( ctor_args )... ) ) {}
+        auto*                    ptr       () const { return content.get(); }
 
-    mutable PI32         instantiated_type_index; ///<
-    PI32                 global_type_index;       ///<
-    char                 data[ data_size ];       ///<
+        mutable PI32             instantiated_type_index; ///<
+        PI32                     global_type_index;       ///<
+        std::unique_ptr<Content> content;                 ///<
+    };
+
+    using        Data       = std::conditional_t<( alignof( Content ) > Object::VfsDtSpec::alig || sizeof( DataDirect ) > sizeof( typename Object::VfsDtSpec ) ), DataHeap, DataDirect>;
+
+    /**/         VfsDtWrap_( auto &&...args ) : data( FORWARD( args )... ) {
+        const auto &type = StaticStorage<VfsDtType<Object,Content,0>>::value;
+        data.instantiated_type_index = type.instantiated_type_index;
+        data.global_type_index = type.global_type_index;
+    }
+
+    Data         data;                    ///<
 };
 
 END_VFS_NAMESPACE
-
-#include "VfsDtWrap.tcc" // IWYU pragma: export
