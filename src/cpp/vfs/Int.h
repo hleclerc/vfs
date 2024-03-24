0ccb5f8cba97e7a1f7c4b6ba722e29f1def07537
Hugo Leclerc <hugal.leclerc@gmail.com>
2024-03-24 06:34:36 +0100

    .

--- src/cpp/vfs/Int.h
+++ src/cpp/vfs/Int.h
@@ -1,31 +1,32 @@
 #pragma once
 
+#include "support/WithDefaultOperators.h"
 #include "Type.h"
 
 BEG_VFS_NAMESPACE
 
-class Scalar : public WithDefaultOperators {
+class Int : public WithDefaultOperators {
 public:
-    VfsDtObject_STD_METHODS( Scalar, "vfs", 4 * sizeof( void * ), alignof( void * ) );
+    STD_METHODS_FOR_VFS_WRAPPER( Int, "VFS_NAMESPACE", "vfs", sizeof( void * ), alignof( void * ) );
 };
 
-// types for ctors --------------------------------------------------------------------------
-auto vfs_dt_impl_type( CtType<Scalar>, const ScalarLike auto &value ) {
-    return CT_DECAYED_TYPE_OF( value );
-}
+// // types for ctors --------------------------------------------------------------------------
+// auto vfs_dt_impl_type( CtType<Scalar>, const ScalarLike auto &value ) {
+//     return CT_DECAYED_TYPE_OF( value );
+// }
 
-inline auto vfs_dt_impl_type( CtType<Scalar> ) {
-    return CtType<CtInt<0>>();
-}
+// inline auto vfs_dt_impl_type( CtType<Scalar> ) {
+//     return CtType<CtInt<0>>();
+// }
 
 
-// -------------------------------------------------------------------------------------------
-// type info
-constexpr inline auto nb_bits_mantissa( CtType<Scalar> ) { return PrimitiveCtInt<1000000>(); }
-constexpr inline auto scalar_class    ( CtType<Scalar> ) { return PrimitiveCtInt<2>(); }
-constexpr inline auto has_sign_bit    ( CtType<Scalar> ) { return PrimitiveCtInt<1>(); }
+// // -------------------------------------------------------------------------------------------
+// // type info
+// constexpr inline auto nb_bits_mantissa( CtType<Scalar> ) { return PrimitiveCtInt<1000000>(); }
+// constexpr inline auto scalar_class    ( CtType<Scalar> ) { return PrimitiveCtInt<2>(); }
+// constexpr inline auto has_sign_bit    ( CtType<Scalar> ) { return PrimitiveCtInt<1>(); }
 
-// scalar_type_for (when creating "virtual" objects from "real" values)
-auto scalar_type_for( PrimitiveCtInt<1>, auto, auto, auto ) { return CtType<Scalar>(); }
+// // scalar_type_for (when creating "virtual" objects from "real" values)
+// auto scalar_type_for( PrimitiveCtInt<1>, auto, auto, auto ) { return CtType<Scalar>(); }
 
 END_VFS_NAMESPACE
